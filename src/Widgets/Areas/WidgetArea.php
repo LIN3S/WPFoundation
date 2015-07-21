e25a670f2123da792747c6e8c51bca02080fa7e6
benatespina <benatespina@gmail.com>
2015-07-21 22:10:54 +0200

    Added Widgets' abstract classes and interfaces

---
+++ src/Widgets/Areas/WidgetArea.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the WPFoundation library.
+ *
+ * Copyright (c) 2015 LIN3S <info@lin3s.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace LIN3S\WPFoundation\Widgets\Areas;
+
+/**
+ * Abstract class of base widget area that implements the interface.
+ * This class avoids the redundant task of create the same WidgetArea constructor.
+ *
+ * @author Beñat Espiña <benatespina@gmail.com>
+ */
+abstract class WidgetArea implements WidgetAreaInterface
+{
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        add_action('widgets_init', [$this, 'widgetArea']);
+    }
+}
